d30b8d36d372c1e2c40c59e80cef7c1a3104a2c7
TheMikelRex <ElFlambMC@users.noreply.github.com>
2014-10-26 20:48:37 +0100

    Update AuthorCommands.java

--- src/main/java/io/sporkpgm/commands/AuthorCommands.java
+++ src/main/java/io/sporkpgm/commands/AuthorCommands.java
@@ -1,25 +1,16 @@
 
 package io.sporkpgm.commands;
-import com.sk89q.minecraft.util.commands.Command;
-import com.sk89q.minecraft.util.commands.CommandContext;
-import com.sk89q.minecraft.util.commands.CommandException;
-import com.sk89q.minecraft.util.commands.CommandPermissions;
-import com.sk89q.minecraft.util.commands.NestedCommand;
-import io.sporkpgm.map.SporkMap;
-import io.sporkpgm.player.SporkPlayer;
-import io.sporkpgm.team.SporkTeam;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class AuthorCommands {
-@Command(aliases = {"author","creador"}, desc = "Shows what team you are on", max = 0)
-public static void myteam(CommandContext cmd, CommandSender sender) throws CommandException {
+@Command(aliases = {"author","creador"}, desc = "Show thee author of the modification of SporkPGM", max = 0)
+public static void author(CommandContext cmd, CommandSender sender) throws CommandException {
 if(!(sender instanceof Player)) {
 throw new CommandException("This command is for Players only");
 }
-SporkPlayer player = SporkPlayer.getPlayer((Player) sender);
-SporkTeam team = player.getTeam();
-sender.sendMessage(ChatColor.GRAY + "You are on " + team.getColoredName());
+sender.sendMessage(ChatColor.RED + "Creador:" + ChatColor.BLUE + "ElFlambMC(TheMikelRex)");
 }
